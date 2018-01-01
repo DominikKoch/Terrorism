81accaa42af03105663132aad4793c0d4128330b
DominikKoch <dominik.w.koch@googlemail.com>
2018-01-01 17:11:34 +0100

    Update FAQ

--- faq.R
+++ faq.R
@@ -1,17 +1,25 @@
 bsCollapse(id = "faq",
+           
   bsCollapsePanel("Where does the data come from?",
-                           p("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", style="text-align: justify;"),
-    a("Intergovernmental Panel on Climate Change GCM guide",
-      href="http://www.ipcc-data.org/guidelines/pages/gcm_guide.html", target="_blank"),
-    style="info"),
+    p("The data is crowdsourced by Wikipedia. You can find ", 
+      a("Lists of terrorist incidents", href = "https://en.wikipedia.org/wiki/List_of_terrorist_incidents#1970%E2%80%93present", target = "_blank"),  
+      "from 1970 to the present. I'm not using the open-source",
+      a("Global Terrorism Database (GTD)", href = "https://www.start.umd.edu/gtd/", target = "_blank"),  
+      "because this data is one year delayed and I want to include the most recent incidents.",
+      style = "text-align: justify;"),
+    style = "info"),
+  
   bsCollapsePanel("How many times is the data updated?",
-                  p("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", style="text-align: justify;"),
-                  a("Intergovernmental Panel on Climate Change GCM guide",
-                    href="http://www.ipcc-data.org/guidelines/pages/gcm_guide.html", target="_blank"),
-                  style="info"),
-  bsCollapsePanel("How did you make this app?",
-    p("ERA 40 is a climate reanalysis data set.", style="text-align: justify;"),
-    a("ERA 40 summary and data access",
-      href="https://climatedataguide.ucar.edu/climate-data/era40", target="_blank"),
-      style="info")
+    p("At the moment this is still a manual process due to occasionally messy Wikipedia data. 
+      Therefore the data will be updated on a regular basis once per month.
+      I might implement a real time data supply in the future."),
+    style = "info"),
+  
+  bsCollapsePanel("How did you make this dashboard?",
+    p("This dashboard is written in", 
+      a("R", href = "https://www.r-project.org/", target = "_blank"), "and built with the", 
+      a("Shiny", href = "https://shiny.rstudio.com/", target = "_blank"), "web applications frame work. 
+      Shiny is an open source R package that provides an elegant and powerful web framework for building web applications without requiring HTML, CSS, or JavaScript knowledge.", 
+      style = "text-align: justify;"), 
+    style = "info")
 )
